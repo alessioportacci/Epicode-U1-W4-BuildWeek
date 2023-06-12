54f70d2f1ef8423fa58f547e5a394ade02f98df5
girardiantonio <girardiantonio98@gmail.com>
2023-06-12 15:32:36 +0200

    Update

---
+++ scripts/welcome.js
@@ -0,0 +1,17 @@
+const form = document.getElementById ("form-proceed")
+const button = document.getElementById ("proceed")
+const check = document.getElementById ("check")
+
+button.addEventListener("click", function(e){
+    e.preventDefault()
+    if (check.checked) {
+        window.location.href = "/test.html" 
+    } else {
+
+    }
+}
+)
+console.log(check)
+console.log(check)
+console.log(check)
+console.log(check)
