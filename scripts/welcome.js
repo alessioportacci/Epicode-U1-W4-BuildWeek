32dfec1270c69d694a0a47e542ba493d6f231aaa
girardiantonio <girardiantonio98@gmail.com>
2023-06-12 16:03:06 +0200

    Update

---
+++ scripts/welcome.js
@@ -0,0 +1,12 @@
+const form = document.getElementById("form-proceed");
+const button = document.getElementById("proceed");
+const check = document.getElementById("check");
+
+// Abbiamo creato le variabili, creiamo le funzioni
+
+button.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (check.checked) {
+    window.location.href = "/test.html";
+  } 
+});
