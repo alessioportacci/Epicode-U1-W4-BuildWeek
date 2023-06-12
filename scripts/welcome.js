f1d8df70fedfcb8d2b0e689e9a2974d0b6946981
Simone Potenza <simonepotenzatr@gmail.com>
2023-06-12 15:37:32 +0200

    Added some comments

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,7 +1,7 @@
 const form = document.getElementById ("form-proceed")
 const button = document.getElementById ("proceed")
 const check = document.getElementById ("check")
-
+// ABbiamo creato le variabili, creiamo le funzioni
 button.addEventListener("click", function(e){
     e.preventDefault()
     if (check.checked) {
